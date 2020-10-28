4a75cfb8d3a2321ffee3e30bcc6809484d5c75be
Ryan Bullock <RBullock@thepsagroup.com.au>
2020-10-28 14:29:55 +1100

    added graph

---
+++ Web Interface/JS/Script.js
@@ -0,0 +1,22 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'Tank Level',
+            borderColor: 'orange',
+            data: [0, 10, 5, 2, 20, 30, 0]
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        responsive: true,
+        maintainAspectRatio: true
+    }
+});
