f8ebf7826f23cbcbf80231b986b201bafb58aed4
Ryan Bullock <RBullock@thepsagroup.com.au>
2020-10-28 17:46:21 +1100

    added node.js to website

--- Web Interface/JS/Script.js
+++ Web Interface/JS/Script.js
@@ -1,3 +1,20 @@
+var startDate = new Date(); //YYYY-MM-DD
+var endDate = new Date() - 7; //YYYY-MM-DD
+
+var getDateArray = function(start, end) {
+    var arr = new Array();
+    var dt = new Date(start);
+    while (dt <= end) {
+        arr.push(new Date(dt));
+        dt.setDate(dt.getDate() + 1);
+    }
+    return arr;
+}
+
+var dateArr = getDateArray(startDate, endDate);
+
+
+
 var ctx = document.getElementById('myChart').getContext('2d');
 
 var chart = new Chart(ctx, {
@@ -6,7 +23,7 @@ var chart = new Chart(ctx, {
 
     // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: timestamps,
         datasets: [{
             label: 'Tank Level',
             borderColor: 'orange',
@@ -21,6 +38,8 @@ var chart = new Chart(ctx, {
 
 
     }
-
-
 });
+
+
+var d = new Date();
+document.getElementById("output").innerHTML = d.getDate + "/" + d.getMonth + "/" + d.getUTCFullYear
